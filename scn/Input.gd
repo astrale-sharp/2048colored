6379827e87c60504a230ce8d09a43bc8f7eb1649
Astrale <ash4567@outlook.fr>
2023-11-30 00:33:57 +0100

    supports smartphone swipes and touch

--- scn/Input.gd
+++ scn/Input.gd
@@ -1,9 +1,12 @@
 extends Node
 class_name InputHandler
+# KeyBoard input and Smartphone input are supported
+
 @onready var timer = $Timer as Timer
 
 signal move(dir : Vector2i)
-	
+func _ready(): set_process_input(true)
+
 func _input(event):
 	if event is InputEventKey:
 		if (get_child(0) as Timer).is_stopped():
@@ -13,5 +16,25 @@ func _input(event):
 		match event.keycode:
 			KEY_LEFT:  move.emit(Vector2i.LEFT)
 			KEY_RIGHT: move.emit(Vector2i.RIGHT)
-			KEY_DOWN: move.emit(Vector2i.DOWN)
-			KEY_UP:   move.emit(Vector2i.UP)
+			KEY_DOWN: move.emit( Vector2i.DOWN)
+			KEY_UP:   move.emit( Vector2i.UP)
+	if event is InputEventSingleScreenSwipe:
+		var dir = event.relative.floor()
+#		dir
+		if abs(dir.x) >= abs(dir.y):
+			dir = Vector2i(sign(dir.x), 0)
+		else :
+			dir = Vector2i(0, sign(dir.y))
+			
+		move.emit(dir)
+		
+	if event is InputEventSingleScreenTouch:
+		var pos : Vector2 = event.position
+		var zero = get_viewport().get_visible_rect().size / 2
+		pos = pos - zero
+		if abs(pos.x) >= abs(pos.y):
+			pos = Vector2i(sign(pos.x), 0)
+		else:
+			pos = Vector2i(0, sign(pos.y))
+		move.emit(pos)
+	
