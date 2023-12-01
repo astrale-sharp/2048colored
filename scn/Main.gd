aa6eb10bef1642ae67ad4c8bc8db9898e0ba5278
Astrale <ash4567@outlook.fr>
2023-12-01 01:50:32 +0100

    adds layout in main

--- scn/Main.gd
+++ scn/Main.gd
@@ -8,10 +8,28 @@ extends Node
 
 
 func _ready():
+	### SELF INIT
 	$Timer.timeout.connect(_on_check_can_play)
-	
 	board_anim.board_size = board.board_size
-
+	
+	
+	### LAYOUT
+	var screen_size = Vector2(
+		ProjectSettings.get_setting("display/window/size/viewport_width"),
+		ProjectSettings.get_setting("display/window/size/viewport_height"),
+	)
+	
+	var cell_size = 0.1 * screen_size.x #if OS.get_name() == "Android" else 64.0
+	var board_anim_size = board.board_size * cell_size + board_anim.gap.x
+	
+	board_anim.cell_size = cell_size
+	
+	board_anim.position.x = screen_size.x / 2 - board_anim_size / 2
+	board_anim.position.y = screen_size.y * 0.1
+	
+	wheel.position.x = screen_size.x * 0.05
+	wheel.position.y = board_anim.position.y + board_anim_size + screen_size.y * 0.05
+	### SIGNAL CONNECTION
 	board.block_moved.connect(   board_anim._on_block_moved   )
 	board.block_fused.connect(   board_anim._on_block_fused   )
 	board.block_created.connect( board_anim._on_block_created )
