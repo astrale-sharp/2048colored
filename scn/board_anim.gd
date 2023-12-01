28ab5645a4e3c879c1b1477b8f290f7653853512
Astrale <ash4567@outlook.fr>
2023-12-01 01:52:14 +0100

    layout refactored in main board animation

--- scn/board_anim.gd
+++ scn/board_anim.gd
@@ -2,34 +2,26 @@
 extends Control
 class_name BoardAnim
 
+## It is assumed in _on_game_over_animation_start that the only children 
+## of this node is the blocks
+
+
+signal animation_finished
 var block := preload("res://scn/block.tscn")
 
 @export var board_size : int
-var cell_size: float = 0.15 * ProjectSettings.get_setting("display/window/size/viewport_width") if OS.get_name() == "Android" else 64
-const gap = Vector2.ONE * 20
+
 var board = {}
+var cell_size: float
+const gap = Vector2.ONE * 20
 
 func _ready():
-	position.x = ProjectSettings.get_setting("display/window/size/viewport_width") / 2 \
-				- ( board_size * cell_size + gap.x)/2
 	if owner == null:
 		board_size = 5
 		set_process_input(true)
 	else:
 		set_process_input(false)
 	if not board_size: printerr("board_size not defined")
-
-	
-
-# test only
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.pressed:
-			var p = (event.position - gap/2) / cell_size
-			p = Vector2i(p)
-			if Vector2i.ZERO <= p and p <= Vector2i.ONE * board_size:
-				if board.get(p) == null:
-					_on_block_created(p, 0)
 
 
 func _draw():
@@ -61,3 +53,29 @@ func _on_block_fused(start : Vector2i, end: Vector2i, level):
 	sblock.move_to((end * cell_size) as Vector2 + gap/2)
 	sblock.fade_out()
 	(board[end] as Block).level_up()
+
+var i 
+func _on_game_over_animation_start():
+	#await get_tree().create_timer(1.2).timeout
+	i = get_child_count()
+	for c in get_children():
+		c = c as Block
+		c.animation_finished.connect(
+			func (): 
+				i -= 1
+				if i == 0: animation_finished.emit()
+		)
+		c.game_over()
+
+
+### TEST only
+func _input(event):
+	return
+	if event is InputEventMouseButton:
+		if event.pressed:
+			var p = (event.position - gap/2) / cell_size
+			p = Vector2i(p)
+			if Vector2i.ZERO <= p and p <= Vector2i.ONE * board_size:
+				if board.get(p) == null:
+					_on_block_created(p, 0)
+
