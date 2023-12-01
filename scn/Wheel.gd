afad7952ddf00a0445cae9cfb8249c40af44371a
Astrale <ash4567@outlook.fr>
2023-12-01 01:48:52 +0100

    wheels game over animation

--- scn/Wheel.gd
+++ scn/Wheel.gd
@@ -1,6 +1,7 @@
 extends Control
 class_name Wheel
 
+signal animation_finished
 @export var amplitude = 10
 #@export 
 @export var heigth_per_block:   int = 40
@@ -13,6 +14,21 @@ var block = preload("res://scn/block.tscn")
 func _on_level_appear(level):
 	index = max(level, index)
 
+func _on_game_over_animation_start():
+	game_over_mode = true
+	await get_tree().create_timer(1.2).timeout
+	
+	set_physics_process(false)
+	
+	await get_tree().create_timer(1.5).timeout
+	
+	for c in get_children():
+		var b: Block = c as Block
+		b.fracture(true)
+	await get_tree().create_timer(3).timeout
+	animation_finished.emit()
+
+
 func _ready():
 	for c in get_children():
 		c.queue_free()
@@ -21,9 +37,10 @@ func _ready():
 		var c = Constants.COLORS[k]
 		var b = block.instantiate() as Block
 		b.color = c
+		b.__prepare_fracture()
 		add_child(b)
 
-var t = 0
+var t = randi_range(0,40000)
 var index := 0:
 	set(val):
 		( get_child(index) as Block ).selected = false
@@ -39,10 +56,19 @@ func _physics_process(delta: float) -> void:
 
 	for k in get_child_count():
 		var c = get_child(k)
-		var a = 0 if not game_over_mode else randf()
+		var a = 0 if not game_over_mode else randf() * 8
 		
 		var ttt = log(tt*tt + 30)* exp( cos(tt + 2*k + 30) )
 #		var ttt = log( 1 + (t*t) ) * exp( cos(t +5*k) ) 
 		c.position = base  \
-					+ heigth_per_block * k * Vector2.DOWN \
+					+ heigth_per_block * k * Vector2.RIGHT \
 					+ amplitude * Vector2(cos(ttt + a), sin(ttt + a))
+
+
+### TEST ONLY
+func _input(event):
+	return
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_F:
+			_on_game_over_animation_start()
+
